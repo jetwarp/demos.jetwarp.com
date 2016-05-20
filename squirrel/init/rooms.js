25cd45b86617f3224f4a7d5446cb182d28e2b292
Stuart P. Bentley <stuart@testtrack4.com>
2016-05-20 01:31:38 +0000

    Add plate rendering

--- squirrel/init/rooms.js
+++ squirrel/init/rooms.js
@@ -1,19 +1,122 @@
 /* global THREE scene panoMaterial setVecFromLatLon
-  location fetch Loquate jsyaml */
+  location fetch Loquate jsyaml canwrap camera */
 
 var scene;
 
 var roomMap = new Map();
 
-var waypointSphereDestinationsByUUID = {};
-var waypointSphereGeometry = new THREE.SphereGeometry(50, 20, 20);
-var waypointSphereMaterial = new THREE.MeshBasicMaterial({color: '#ff0000'});
-var roomGeometry = new THREE.SphereGeometry(500, 60, 40);
+var roomRadius = 4096;
+var roomGeometry = new THREE.SphereGeometry(roomRadius, 60, 40);
   roomGeometry.scale(-1, 1, 1);
 var cdnPath;
 
 var loader = new THREE.TextureLoader();
 loader.crossOrigin = 'anonymous';
+
+var plateWidth = 512;
+var titleFont = '800 40px Open Sans';
+var bodyFont = '400 20px Open Sans';
+var plateMargin = 12;
+// These next two are admittedly kind of clumsy
+var titleLineHeight = 40;
+var bodyLineHeight = 25;
+
+var bgFillStyle = 'rgba(224, 224, 224, 0.875)';
+var textFillStyle = 'black';
+
+var plateTextWidth = plateWidth - plateMargin * 2;
+
+// The pixels-to-world-units scale for the geometry
+var plateScale = 0.25;
+// How far away to place plates
+var plateDistance = 128;
+
+function nextPowerOfTwo(x) {
+  return Math.pow(2, Math.ceil(Math.log(x) / Math.log(2)));
+}
+
+function makePlate(plate) {
+  // TODO: figure out a way for each plate to not need its own canvas
+  // see https://github.com/jetwarp/demos.jetwarp.com/issues/6
+  var plateRenderCanvas = document.createElement('canvas');
+  plateRenderCanvas.width = plateWidth;
+  var ctx = plateRenderCanvas.getContext('2d');
+  var textWrapper = new canwrap(ctx);
+  // TODO: only split lines once
+  // (this is a change that would need to happen in canwrap upstream)
+  ctx.font = bodyFont;
+  var bodyLineCount = textWrapper.splitLines(
+    plate.body, plateTextWidth).length;
+  var bodyTop = plate.title ? titleLineHeight + plateMargin * 2 : plateMargin;
+
+  var plateHeight = bodyTop + bodyLineCount * bodyLineHeight + plateMargin;
+  plateRenderCanvas.height = nextPowerOfTwo(plateHeight);
+
+  // TODO: Probably not this, when using shader for background
+  // https://github.com/jetwarp/demos.jetwarp.com/issues/7
+
+  // NOTE: This globalCompositeOperation trickery is kind of redundant
+  // when we're not reusing the canvas, since it should start empty anyway,
+  // but we're doing it anyway Because It's The Right Thing To Do
+  ctx.globalCompositeOperation = 'copy';
+  ctx.fillStyle = bgFillStyle;
+  ctx.fillRect(0, 0, plateRenderCanvas.width, plateRenderCanvas.height);
+  ctx.globalCompositeOperation = 'source-over';
+
+  ctx.fillStyle = textFillStyle;
+
+  // TODO: Text rendering should probably block
+  // until we're sure the font is ready
+  ctx.font = bodyFont;
+  textWrapper.fillWrappedText(
+    plate.body, plateMargin, bodyTop + bodyLineHeight,
+    plateTextWidth, bodyLineHeight);
+
+  if (plate.title) {
+    ctx.font = titleFont;
+    ctx.fillText(plate.title, plateMargin, plateMargin + titleLineHeight,
+      // because we want the title to fit one line
+      plateTextWidth);
+  }
+
+  var plateGeometry = new THREE.PlaneGeometry(
+    plateWidth * plateScale, plateHeight * plateScale);
+
+  // Cut the UVs so they end at the bottom of the texture
+  // TODO: Maybe scale the texture instead? Depends on how issue #6 shakes out
+  var uvs = plateGeometry.faceVertexUvs[0]
+  uvs[0][1].y = uvs[1][0].y = uvs[1][1].y =
+    1 - plateHeight / plateRenderCanvas.height;
+
+  var plateTexture = new THREE.Texture(plateRenderCanvas);
+  // For some reason we need to update the texture we just created
+  // https://github.com/mrdoob/three.js/issues/8939
+  plateTexture.needsUpdate = true;
+
+  var plateMaterial = new THREE.MeshBasicMaterial({map: plateTexture});
+  plateMaterial.transparent = true;
+
+  var plateMesh = new THREE.Mesh(plateGeometry, plateMaterial);
+  setVecFromLatLon(plateMesh.position, plate.lat, plate.lon)
+    .multiplyScalar(plateDistance);
+  plateMesh.lookAt(camera.position);
+
+  return plateMesh;
+}
+
+var waypointDistance = 512;
+
+var waypointSphereDestinationsByUUID = {};
+var waypointSphereGeometry = new THREE.SphereGeometry(50, 20, 20);
+var waypointSphereMaterial = new THREE.MeshBasicMaterial({color: '#ff0000'});
+
+function makeWaypointSphere(waypoint) {
+  var waypointSphere = new THREE.Mesh(
+    waypointSphereGeometry, waypointSphereMaterial);
+  setVecFromLatLon(waypointSphere.position, waypoint.lat, waypoint.lon)
+    .multiplyScalar(waypointDistance);
+  return waypointSphere;
+}
 
 function populateRoom(roomObj) {
   var roomName = roomObj.room;
@@ -25,18 +128,30 @@ function populateRoom(roomObj) {
   var mesh = new THREE.Mesh( roomGeometry, panoMaterial );
   roomGroup.add( mesh );
 
-  var waypointSpheres = new THREE.Object3D();
-  for (var i = 0; i < roomObj.waypoints.length; i++) {
-    var waypoint = roomObj.waypoints[i];
-    var waypointSphere = new THREE.Mesh(
-      waypointSphereGeometry, waypointSphereMaterial);
-    setVecFromLatLon(waypointSphere.position, waypoint.lat, waypoint.lon);
-    waypointSphere.updateMatrixWorld();
-    waypointSphereDestinationsByUUID[waypointSphere.uuid] = waypoint.dest;
-    waypointSpheres.add(waypointSphere);
+  if (roomObj.waypoints) {
+    var waypointSpheres = new THREE.Object3D();
+    for (var i = 0; i < roomObj.waypoints.length; i++) {
+      var waypoint = roomObj.waypoints[i];
+      var waypointSphere = makeWaypointSphere(waypoint);
+      waypointSphereDestinationsByUUID[waypointSphere.uuid] = waypoint.dest;
+      waypointSpheres.add(waypointSphere);
+    }
+    roomObj.waypointSpheres = waypointSpheres;
+    roomGroup.add(waypointSpheres);
   }
-  roomObj.waypointSpheres = waypointSpheres;
-  roomGroup.add(waypointSpheres);
+
+  if (roomObj.plates) {
+    var plateGroup = new THREE.Object3D();
+    for (var i = 0; i < roomObj.plates.length; i++) {
+      var plate = roomObj.plates[i];
+      var plateMesh = makePlate(plate);
+      setVecFromLatLon(waypointSphere.position, waypoint.lat, waypoint.lon)
+        .multiplyScalar(waypointDistance);
+      plateGroup.add(plateMesh);
+    }
+    roomGroup.add(plateGroup);
+  }
+
   roomObj.group = roomGroup;
 }
 
--- squirrel/lib/latLon.js
+++ squirrel/lib/latLon.js
@@ -6,7 +6,9 @@ function setVecFromLatLon(vec, lat, lon) {
   var phi = THREE.Math.degToRad( 90 - lat );
   var theta = THREE.Math.degToRad( lon );
 
-  vec.x = 500 * Math.sin( phi ) * Math.cos( theta );
-  vec.y = 500 * Math.cos( phi );
-  vec.z = 500 * Math.sin( phi ) * Math.sin( theta );
+  vec.x = Math.sin( phi ) * Math.cos( theta );
+  vec.y = Math.cos( phi );
+  vec.z = Math.sin( phi ) * Math.sin( theta );
+
+  return vec;
 }
